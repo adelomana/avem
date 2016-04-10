ddca0924f2e24e5e107e248d5a5ddd8eac8cbe15
Adrian de Lomana <adriandelomana@necio4.PK5001Z>
2016-04-09 19:42:41 -0700

    update for only juv data

--- bufferEffectAnalyser.r
+++ bufferEffectAnalyser.r
@@ -1,84 +1,60 @@
 ### this script computes for each species the effect of buffer size. to be run as source("bufferEffectAnalyser.r")
                                         # 0.1. user defined paths
 setwd <- getwd()
-pfem <- read.csv("data/P3-HS_thre_AUC.csv")
 pjuv <- read.csv("data/P4-HS_thre_AUC.csv")
                                         # 0.2. loading common functions
 source("commonFunctions.r")
                                         # MAIN
                                         # 1. defining the species
-species.list <- levels(pfem$Species)
+species.list <- levels(pjuv$Species)
                                         # for each species
 for(i in 1:length(species.list)){
 #for(i in 1:1){
     working.species <- species.list[i]
     print(working.species)
                                         # 2. computing model
-    print("computing model for pfem...")
-    model.pfem <- model.calculator(fitness.data=pfem,working.species,label="patch")
-    print(model.pfem)
     print("computing model for juv...")
     model.juv <- model.calculator(fitness.data=pjuv,working.species,label="age")
     print(model.juv)
                                         # 3. making plot for each species
     plot.file.name <- paste("figures/be_",working.species,".pdf",sep="")
     pdf(plot.file.name)
-    shift <- 0.2
+    
                                         # 3.1. defining the plotting variables
     x <- c(1,2,4,8,12,16)
 
-    y.pfem <- model.pfem$estimates
-    z.pfem <- model.pfem$estimate.errors
     y.juv <- model.juv$estimates
     z.juv <- model.juv$estimate.errors
 
                                         # dealing with less buffers part 1
-    subsetIndexes <- c(1,3,6)
-    subsetIndexes <- c(1,2,3,4,5,6) # forcing all buffers
+    subsetIndexes <- c(1,2,3,4,6) # forcing all buffers, but 12
     x <- x[subsetIndexes]
-    y.pfem <- y.pfem[subsetIndexes]
-    z.pfem <- z.pfem[subsetIndexes]
     y.juv <- y.juv[subsetIndexes]
     z.juv <- z.juv[subsetIndexes]
                                         # end of dealing with less buffers part 1
-    
-    top.pfem <- max(y.pfem+2*z.pfem)
-    bottom.pfem <- min(y.pfem-2*z.pfem)
-
     top.juv <- max(y.juv+2*z.juv)
     bottom.juv <- min(y.juv-2*z.juv)
 
-    the.ylim <- c(min(c(bottom.pfem,bottom.juv)),max(c(top.pfem,top.juv))+0.2*max(c(top.pfem,top.juv)))
+    the.ylim <- c(min(c(bottom.juv)),max(c(top.juv))+0.2*max(c(top.juv)))
     the.ylim <- c(-0.6,1.4) # forcing the ylim 
     the.xlim <- c(0,22)
 
                                         # this block should be after ylim has been properly defined, otherwise the position of the stars will be misplaced
-    w.pfem <- 0; w.juv <- 0
-    w.pfem[(model.pfem$pvalues < 0.05) & (y.pfem > 0.)] <- the.ylim[2]-0.01*the.ylim[2]
-    w.pfem[(model.pfem$pvalues < 0.05) & (y.pfem < 0.)] <- the.ylim[1]+0.01*the.ylim[1]
-    w.pfem[model.pfem$pvalues >= 0.05] <- 100
+    w.juv <- 0
     w.juv[(model.juv$pvalues < 0.05) & (y.juv > 0.)] <- the.ylim[2]-0.01*the.ylim[2]
     w.juv[(model.juv$pvalues < 0.05) & (y.juv < 0.)] <- the.ylim[1]+0.01*the.ylim[1]
-    w.juv[model.juv$pvalues >= 0.05] <- 100
+    w.juv[model.juv$pvalues >= 0.05] <- 100    
 
-    
                                         # dealing with less buffers part 2
-    w.pfem <- w.pfem[subsetIndexes]
     w.juv <- w.juv[subsetIndexes]
                                         # dealing with less buffers part 2
 
                                         # 3.2. actual plotting
-    plot(x-shift,y.pfem,pch=20,xlim=the.xlim,ylim=the.ylim,xlab="buffer size",ylab="estimate",main=working.species,col="blue",panel.first=abline(h=0,col="gray60"))
-    segments(x-shift,y.pfem-z.pfem,x-shift,y.pfem+z.pfem,lwd=2,col="blue")
-    segments(x-shift,y.pfem-2*z.pfem,x-shift,y.pfem+2*z.pfem,lwd=1,col="blue")
-    par(new=TRUE)
-    plot(x+shift,y.juv,pch=20,col="red",xlim=the.xlim,ylim=the.ylim,xlab="",ylab="")
-    segments(x+shift,y.juv-z.juv,x+shift,y.juv+z.juv,lwd=2,col="red")
-    segments(x+shift,y.juv-2*z.juv,x+shift,y.juv+2*z.juv,lwd=1,col="red")
+    plot(x,y.juv,pch=20,xlim=the.xlim,ylim=the.ylim,xlab="buffer size",ylab="estimate",main=working.species,col="black",panel.first=abline(h=0,col="gray60"))
+    segments(x,y.juv-z.juv,x,y.juv+z.juv,lwd=2,col="black")
+    segments(x,y.juv-2*z.juv,x,y.juv+2*z.juv,lwd=1,col="black")
 
-    legend(18,the.ylim[2],c("pfem","juv"),lty=c(1,1),lwd=c(2.5,2.5),pch=20,col=c("blue","red"))
-    points(x-shift,w.pfem,col="blue",pch=8)
-    points(x+shift,w.juv,col="red",pch=8)
+    points(x,w.juv,col="black",pch=8)
 
     dev.off()  
 }             
--- estimateAnalyser.r
+++ estimateAnalyser.r
@@ -1,79 +1,33 @@
 ### this script computes the estimate effect of habitat and productivity for each species
                                         # 0.1. user defined paths
 setwd <- getwd()
-pfem <- read.csv("data/P3-HS_thre_AUC.csv")
 juv <- read.csv("data/P4-HS_thre_AUC.csv")
                                         # 0.2. loading common functions
 source("commonFunctions.r")
 
                                         # MAIN
                                         # 1. defining some initial variables
-species.list <- levels(pfem$Species)
-species.pfem.models <- data.frame()
+species.list <- levels(juv$Species)
 species.juv.models <- data.frame()
                                         # for each species 
 for(i in 1:length(species.list)){
+#for(i in 1:1){
     working.species <- species.list[i]
     print(working.species)
                                         # 2. computing model
-    print("computing model for pfem...")
-    model.pfem <- model.calculator(fitness.data=pfem,working.species,label="patch")
-    print(model.pfem)
     print("computing model for juv...")
     model.juv <- model.calculator(fitness.data=juv,working.species,label="age")
     print(model.juv)
                                         # 3. appending the computed models into a list
-    species.pfem.models <- rbind( species.pfem.models, data.frame(working.species,model.pfem) )
     species.juv.models <- rbind( species.juv.models, data.frame(working.species,model.juv) )
 }
                                         # 4. building a figure for each buffer size
-bufferNames <- colnames(pfem)[9:14]
+bufferNames <- colnames(juv)[c(9,10,11,12,14)]
+
 for (indexBuffer in  1:length(bufferNames)){
     bufferTag <- bufferNames[indexBuffer]
-                                        # 4.1. defining the pfem plot
-    figureFileName <- paste("figures/estimates_pfem_",bufferTag,".pdf",sep="")
-    pdf(figureFileName)
-    estimates <- c()
-    estimate.errors <- c()
-    plotLabels <- c()
-    w <- c()
-    for(i in 1:length(species.list)){
-        estimate <- species.pfem.models[species.pfem.models$working.species == species.list[i],]$estimates[indexBuffer]
-        estimates <- c(estimates,c(estimate))
-        estimate.error <- species.pfem.models[species.pfem.models$working.species == species.list[i],]$estimate.errors[indexBuffer]
-        estimate.errors <- c(estimate.errors,c(estimate.error))
-        plotLabels <- c(plotLabels,c(species.list[i]))
-        pvalue <- species.pfem.models[species.pfem.models$working.species == species.list[i],]$pvalues[indexBuffer]
-        w <- c(w,c(pvalue))
-    }
-
-    if (indexBuffer == 1) {
-        pfemOrderedIndexes <- order(estimates,decreasing=TRUE)
-    }
-    estimates <- estimates[pfemOrderedIndexes]
-    estimate.errors <- estimate.errors[pfemOrderedIndexes]
-    plotLabels <- plotLabels[pfemOrderedIndexes]
-    w <- w[pfemOrderedIndexes] 
-    
-    #the.xlim = c(min(estimates - 2*estimate.errors)-0.1*min(estimates - 2*estimate.errors), max(estimates + 2*estimate.errors)+0.1*max(estimates + 2*estimate.errors))
-    the.xlim = c(-0.35,1.5) # forcing the x limit
-    thexlab = bufferTag
-    theylab = ""
-    plot(x=estimates,length(estimates):1, pch = 20, ylim = c(0, length(estimates)+1),xlab=thexlab,ylab=theylab,xlim=the.xlim,yaxt="n",main="pfem")
-    abline(v=0, col = "grey50")
-    segments( estimates-estimate.errors, length(estimates):1, estimates+estimate.errors, length(estimates):1, lwd = 2)
-    segments( estimates-2*estimate.errors, length(estimates):1, estimates+2*estimate.errors, length(estimates):1, lwd = 1)
-    axis(side = 2, at = length(estimates):1, labels = plotLabels, tick = FALSE, las = 1)
-
-    z <- 0
-    z[(w < 0.05) & (estimates > 0.)] <- the.xlim[2]-0.05*the.xlim[2]
-    z[(w < 0.05) & (estimates < 0.)] <- the.xlim[1]+0.05*the.xlim[1]
-    z[w >= 0.05] <- 100
-
-    points(z,length(estimates):1,pch=8)
-
-    dev.off()
-                                        # 4.2. defining the juv plot
+                                        
+                                        # 4.1. defining the juv plot
     figureFileName <- paste("figures/estimates_juv_",bufferTag,".pdf",sep="")
     pdf(figureFileName)
     estimates <- c()
